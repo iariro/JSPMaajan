84f12c49fcb1d035afdfb3911f801a62c6b14ac8
kumagai <iariro@gol.com>
2024-03-13 21:27:49 +0900

    データ誤り修正

--- src/kumagai/Maajan/logictest/tenpai/TenpaiPatternTest2.java
+++ src/kumagai/Maajan/logictest/tenpai/TenpaiPatternTest2.java
@@ -12,36 +12,36 @@ public class TenpaiPatternTest2
 	public void test202双石並1()
 		throws Exception
 	{
-		executeTest(TenpaiData2.data201);
+		executeTest(TenpaiData2.data202);
 	}
 
 	public void test203ノベタン()
 		throws Exception
 	{
-		executeTest(TenpaiData2.data201);
+		executeTest(TenpaiData2.data203);
 	}
 
 	public void test204亜両面()
 		throws Exception
 	{
-		executeTest(TenpaiData2.data201);
+		executeTest(TenpaiData2.data204);
 	}
 
 	public void test205変則二面待ち()
 		throws Exception
 	{
-		executeTest(TenpaiData2.data201);
+		executeTest(TenpaiData2.data205);
 	}
 
 	public void test206変則二面待ち()
 		throws Exception
 	{
-		executeTest(TenpaiData2.data201);
+		executeTest(TenpaiData2.data206);
 	}
 
 	public void test207変則二面待ち()
 		throws Exception
 	{
-		executeTest(TenpaiData2.data201);
+		executeTest(TenpaiData2.data207);
 	}
 }
